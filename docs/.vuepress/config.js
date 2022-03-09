63e7438573e47dc2d87c721e735c26ae368abb6d
wangpengli <2422418942@qq.com>
2022-03-09 12:05:18 +0800

    更改标题和描述

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,8 +2,8 @@ module.exports = {
   // 站点配置
   head: [['link', { rel: 'icon', href: '/images/favicon.ico' }]],
   lang: 'zh-CN',
-  title: '你好， VuePress ！',
-  description: '这是我的第一个 VuePress 站点',
+  title: '代码搬运工',
+  description: '我们不生产轮子，我们只是代码的搬运工',
 
   // 主题和它的配置
   theme: '@vuepress/theme-default',
